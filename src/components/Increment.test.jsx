831a8932640ae4ca37da280e9129d4aa4d852c5e
Arjun KayalMoni <arjun.umathanu@gmail.com>
2023-06-13 03:43:43 +0530

    Checked that react-testing-library is working fine using a basic test and logRoles works fine too

--- src/components/Increment.test.jsx
+++ src/components/Increment.test.jsx
@@ -1,3 +1,4 @@
+import { render, screen, logRoles } from '@testing-library/react';
 import Increment from './Increment';
 
 describe('something truthy and falsy', () =>
@@ -10,3 +11,14 @@ describe('something truthy and falsy', () =>
     expect(false).toBe(false);
   });
 });
+
+describe.only('<Increment />', () =>
+{
+    it('renders the component correctly', async () => {
+    //    const {container} = render(<Increment />)
+    //    logRoles(container)
+        render(<Increment />)
+        const h3 = await screen.findByRole('heading', { level: 3 })
+        expect(h3).toBeInTheDocument()
+    });
+ })
