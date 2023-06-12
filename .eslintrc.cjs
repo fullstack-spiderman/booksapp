f7df97ffd4619c6a2138d9faaa195606c58ce5ef
Arjun KayalMoni <arjun.umathanu@gmail.com>
2023-06-13 03:30:43 +0530

    Extended and integrated plugins config with eslint and added their respective rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,35 @@
+/* eslint-env node */
+
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    'vitest-globals/env': true
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
+    'plugin:vitest-globals/recommended'
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'testing-library', 'jest-dom'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'testing-library/await-async-query': 'error',
+		'testing-library/no-await-sync-query': 'error',
+		'testing-library/no-debugging-utils': 'warn',
+    'testing-library/no-dom-import': 'off',
+    'jest-dom/prefer-checked': 'error',
+    'jest-dom/prefer-enabled-disabled': 'error',
+    'jest-dom/prefer-required': 'error',
+    'jest-dom/prefer-to-have-attribute': 'error',
   },
 }
